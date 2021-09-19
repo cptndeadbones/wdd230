5b13b0092fa76a62ec6ea22715c6a3a4760aed0f
cptndeadbones <pro14001@byui.edu>
2021-09-18 23:49:52 -0400

    Footer stylings

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ let d = new Date();
 let year = d.getFullYear();
 
 document.getElementById("currentYear").textContent = year;
+
+let updated = document.lastModified;
+document.getElementById("lastUpdated").innerHTML= updated;
