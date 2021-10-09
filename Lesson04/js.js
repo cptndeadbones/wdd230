b4618c4794d970ff10a3d19783366696bb6c5b76
cptndeadbones <pro14001@byui.edu>
2021-10-09 15:59:03 -0400

    Preston page updates

---
+++ Lesson04/js.js
@@ -0,0 +1,11 @@
+let d = new Date();
+let year = d.getFullYear();
+
+document.getElementById("currentYear").textContent = year;
+
+let updated = document.lastModified;
+document.getElementById("lastUpdated").innerHTML= updated;
+
+/************************************************************
+ * Responsive hamburger menu
+ ***********************************************************/
