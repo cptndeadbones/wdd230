89a6a4ac307896e1a76b2dbadf7d2c5b1e97e96f
cptndeadbones <pro14001@byui.edu>
2021-11-13 16:49:58 -0500

    changes

--- lesson09/javascript/home.js
+++ lesson09/javascript/home.js
@@ -1,31 +1,49 @@
 const homepageurl = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-fetch(requestURL)
+fetch(homepageurl)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    const towns = jsonObject['town'];
+    const towns = jsonObject['towns'];
     towns.forEach((city) => {
+        //section and html element definition and creation.
+        if (city.name == "Soda Springs" || city.name == "Fish Haven" || city.name == "Preston"){
         let section = document.createElement('section');
         let h2 = document.createElement('h2');
         let h3 = document.createElement('h3');
         let year = document.createElement('p');
         let population = document.createElement('p');
-        let anr = document.createElement('p');//anr = annual rain fall. 
+        let anr = document.createElement('p'); //anr = annual average rain fall. 
         let image = document.createElement('img');
 
-        h2.innerHTML = `${town.name}`;
+        //JSON import and assembly for html section.
+        h2.innerHTML = `${city.name}`;
         section.appendChild(h2);
-        h3.innerHTML = `${town.motto}`;
+        h3.innerHTML = `${city.motto}`;
         section.appendChild(h3);
+        year.innerHTML = `Year founded: ${city.yearFounded}`;
+        section.appendChild(year);
+        population.innerHTML = `Current Pop. ${city.currentPopulation}`;
+        section.appendChild(population);
+        anr.innerHTML = `Average Rainfall / Yr: ${city.averageRainfall}in.`;
+        section.appendChild(anr);
+        if (city.name == "Soda Springs") {
+            image.setAttribute('src', '/wdd230/lesson09/images/instagramicon.png'); // How do we source our own image? 
+            image.setAttribute('alt', `${city.name}`);
+            section.appendChild(image);
+        } else if( city.name == "Fish Haven"){
+            image.setAttribute('src', '/wdd230/lesson09/images/facebookicon.png'); // How do we source our own image? 
+            image.setAttribute('alt', `${city.name}`);
+            section.appendChild(image);
+        } else if(city.name == "Preston"){
+            image.setAttribute('src', '/wdd230/lesson09/images/facebookicon.png'); // How do we source our own image? 
+            image.setAttribute('alt', `${city.name}`);
+            section.appendChild(image);
+        }
 
-        birthday.innerHTML = `Date of Birth: ${prophet.birthdate}<br><br> Place of Birth: ${prophet.birthplace}`;
-        section.appendChild(birthday);
-        image.setAttribute('src', prophet.imageurl);
-        image.setAttribute('alt', `${prophet.name} ${prophet.lastname}-${prophet.order}`);
-        section.appendChild(image);
 
-        document.querySelector('div.cards').appendChild(section);
+        document.querySelector('div.featured-towns').appendChild(section);
+        }
       });
 });
