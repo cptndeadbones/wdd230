660cf232637d279167bfb8fadb97245bf68a2c39
cptndeadbones <pro14001@byui.edu>
2021-11-18 22:44:15 -0500

    open

---
+++ lesson08/javascript/rating.js
@@ -0,0 +1,6 @@
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+    // if (ratingvalue == 1){
+    //     ratingvalue.text.style.color = "red";
+    // }
+}
