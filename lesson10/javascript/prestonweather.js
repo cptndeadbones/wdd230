885414fe13033c25212b72223aa26a59793043b7
cptndeadbones <pro14001@byui.edu>
2021-11-21 00:55:52 -0500

    updates

--- lesson10/javascript/prestonweather.js
+++ lesson10/javascript/prestonweather.js
@@ -3,11 +3,12 @@ fetch(weatherapi)
     .then((response) => response.json())
     .then((jsObject) => {
         windChill(jsObject)
+        const imagesrc = "http://openweathermap.org/img/w/" + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
         document.getElementById('current').innerHTML = jsObject.weather[0].description;
         document.getElementById('high').innerHTML = Math.floor(jsObject.main.temp);
         document.getElementById('humidity').innerHTML = jsObject.main.humidity;
         document.getElementById('wind-speed').innerHTML = jsObject.wind.speed;
-    });
 
     windy = document.getElementById("high");
     function windChill(jsObject) {
@@ -21,3 +22,34 @@ fetch(weatherapi)
         f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
         return f.toFixed(1); }
     windChill();
+
+    const forecastapi = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=56f32390d192e8a8ff885303329930ca";
+    fetch(forecastapi)
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+            return response.json();
+          }})
+        .then((jsObject) => {
+        const sixpm = jsObject.list.filter(x => x.dt_txt.includes('18:00:00')); 
+        let day = 1; 
+        sixpm.forEach(evening => {
+            let date = new Date(evening.dt_txt);
+            const weekdays = [
+                "Sun",
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thur",
+                "Fri",
+                "Sat"];
+                document.getElementById(`day${day}`).innerHTML = weekdays[date.getDay()];
+                document.getElementById(`img${day}`).setAttribute('src', imagesrc);
+                document.getElementById(`img${day}`).setAttribute('alt', desc);
+                document.getElementById(`temp${day}`).innerHTML = Math.floor(evening.main.temp)+`&deg;F`;
+                day++;
+            
+        })
+    })
+});
