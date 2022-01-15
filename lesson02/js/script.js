3e34b53ca08589bad15cb0f640f18ac7d96a7226
cptndeadbones <pro14001@byui.edu>
2022-01-15 11:26:04 -0500

    pictures update

---
+++ lesson02/js/script.js
@@ -0,0 +1,7 @@
+let d = new Date();
+let year = d.getFullYear();
+
+document.getElementById("currentYear").textContent = year;
+
+let updated = document.lastModified;
+document.getElementById("lastUpdated").innerHTML= updated;
