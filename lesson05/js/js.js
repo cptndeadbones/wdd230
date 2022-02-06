bbc2741be6de3a76d6fbe4ce644b6f2a97aa1ac1
cptndeadbones <pro14001@byui.edu>
2022-02-05 20:40:28 -0500

    updating cards on the home page.

---
+++ lesson05/js/js.js
@@ -0,0 +1,38 @@
+let d = new Date();
+let year = d.getFullYear();
+
+document.getElementById("currentYear").textContent = year;
+
+let updated = document.lastModified;
+document.getElementById("lastUpdated").innerHTML= updated;
+
+/************************************************************
+ * Responsive hamburger menu
+ ***********************************************************/
+const hamburger = document.getElementsByClassName("burger")[0];
+
+function toggleNavMenu() {
+    hamburger.classList.toggle('hide');
+}
+
+
+/************************************************************
+ * Current Date output.
+ **********************************************************/
+let date = new Date ();
+document.getElementById("currentDate").innerHTML = date.toDateString();
+
+/************************************************************
+ * Current Date output for header. 
+ **********************************************************/
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+let day = weekday[d.getDay()];
+let mon = month[d.getUTCMonth()];
+
+document.getElementById("weekday").innerHTML = day;
+document.getElementById("day").innerHTML = d.getDate();
+document.getElementById("month").innerHTML = mon;
+document.getElementById("year").innerHTML = d.getFullYear();
+
