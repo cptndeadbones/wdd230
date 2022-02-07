71729ca6ddcb665792ef73d92d123033817a1dfb
cptndeadbones <pro14001@byui.edu>
2022-02-06 21:45:04 -0500

    updates

---
+++ lesson05/js/announcment.js
@@ -0,0 +1,9 @@
+const eggs = d.getDay();
+let saturdayMsg = document.querySelector(".pancakes");
+
+function breakfast(){
+  if (eggs === 1 || eggs === 2){
+      saturdayMsg.classList.remove("syrup");
+    } else { saturdayMsg.classList.add("syrup");
+    }
+}
