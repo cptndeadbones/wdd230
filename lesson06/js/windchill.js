b0369d4f3a1f352240542209e29e57b7d7e9c8b8
cptndeadbones <pro14001@byui.edu>
2022-02-26 17:56:41 -0500

    js hidden update

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -1,12 +1,13 @@
 function windChill() {
-    let tempF = parseInt(document.getElementById('high').innerHTML);
-    let speed = parseInt(document.getElementById('wind-speed').innerHTML);
+    let tempF = parseInt(document.getElementById('high'));
+    let speed = parseInt(document.getElementById('wind-speed'));
     let chill = calculateChill(tempF, speed);
     if ((tempF <= 50) && (speed >= 3.0)){ document.getElementById('wind-chill').innerHTML = chill + "\u00B0F"; }
-    else { document.getElementById('wind-chill').innerHTML = "N/A"; }
+    else { document.getElementById('wind-chill').innerHTML = "N/A";
+}
 }
 function calculateChill(tempF, speed) {
     f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
     return f.toFixed(1); }
 
-    windChill()
+    windChill();
