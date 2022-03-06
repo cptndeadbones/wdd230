abc454ee4ceba0b8945b33c539cddb3c96b69b0a
cptndeadbones <pro14001@byui.edu>
2022-03-05 22:51:51 -0500

    JSON update / directory js build

---
+++ lesson09/javascript/directory.js
@@ -0,0 +1,27 @@
+const requestURL = 'https://cptndeadbones.github.io/wdd230/lesson09/json/data.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const localBusiness = jsonObject['localBusiness'];
+    localBusiness.forEach((business) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let address = document.createElement('p');
+        let image = document.createElement('img');
+
+        image.setAttribute('src', business.image);
+        image.setAttribute('alt', `${business.iconAlt}`);
+        card.appendChild(image);
+        h2.innerHTML = `Name: ${business.name}`;
+        card.appendChild(h2);
+        address.innerHTML = `Address: ${business.address}`;
+        card.appendChild(address);
+
+
+        document.querySelector('div.bizcards').appendChild(card);
+      });
+});
