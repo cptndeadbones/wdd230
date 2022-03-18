fed95cdcfcb5125881864c3793f1ac954b98e6f3
cptndeadbones <pro14001@byui.edu>
2022-03-18 00:17:10 -0400

    weather tile update

--- lesson10/javascript/weather.js
+++ lesson10/javascript/weather.js
@@ -4,6 +4,12 @@ fetch(apiUrl)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-  });
-
-  
+    document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(1);
+    
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('.rainy').textContent = desc;
+    document.querySelector('#wind-speed').textContent = jsObject.wind.speed;
+});
