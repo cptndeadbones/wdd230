2ef27f0b4c0ebcfd713d32b91f46534c599d7d7c
cptndeadbones <pro14001@byui.edu>
2022-03-18 00:29:20 -0400

    ssl on api fetch

--- lesson10/javascript/weather.js
+++ lesson10/javascript/weather.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&appid=56f32390d192e8a8ff885303329930ca";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&appid=56f32390d192e8a8ff885303329930ca";
 
 fetch(apiUrl)
   .then((response) => response.json())
